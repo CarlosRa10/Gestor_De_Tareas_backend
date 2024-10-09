f06b3e2bf2d435ac95e48896fa9954a78a7b46ce
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-10-09 16:11:15 -0400

    commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import colors from 'colors'
+import server from './server'
+
+//definicion del puerto y si no esta disponible le asignamos el 4000
+//esta variable de entorno se inyecta en automatico cuando haces el deployment
+//si no existe el de deployment asignamos un puerto
+const port = process.env.PORT || 4000
+
+server.listen(port,(()=>{
+    console.log(colors.bgGreen.bold(`REST API funcionando en el puerto ${port}`))
+}))
