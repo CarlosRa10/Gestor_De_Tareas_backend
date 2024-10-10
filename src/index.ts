6f026acbc8b93577aa59553f0f6bef1f8a175cd4
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-10-10 16:56:41 -0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ import server from './server'
 const port = process.env.PORT || 4000
 
 server.listen(port,(()=>{
-    console.log(colors.bgGreen.bold(`REST API funcionando en el puerto ${port}`))
+    console.log(colors.cyan.bold(`REST API funcionando en el puerto ${port}`))
 }))
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
+//server.ts es el punto de entrada principal. Al llamarlo allí, te aseguras de que todas las variables de entorno estén disponibles para cualquier archivo que las necesite.
 //config del servidor
 import express from 'express';
+import dotenv from 'dotenv'
+import { connectDB } from './config/db';
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
