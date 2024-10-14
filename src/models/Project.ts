39512e219018179642a5bec2c64761ab76702efe
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-10-14 10:21:38 -0400

    commit

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,5 +1,6 @@
 //modelos: la forma que va a tener los datos en tus bases de datos
-import mongoose, {Schema, Document} from "mongoose";
+import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
+import { ITask } from "./Task";
 
 //Document: Este es un tipo que proviene generalmente de Mongoose (o de la biblioteca que estés usando). Representa un documento de MongoDB, lo que significa que este tipo ya incluye todos los métodos y propiedades que tiene un documento en la base de datos.
 //&: Este símbolo se usa para la intersección de tipos. Significa que ProjectType no solo es un Document, sino que también puede incluir propiedades adicionales que se definan en el objeto vacío {}.
@@ -15,6 +16,7 @@ export interface IProject extends Document  {
     projectName: string
     clientName: string
     description: string
+    tasks: PopulatedDoc<ITask & Document>[]//ITask-hace referencia a decirle que vamos a almacenar en esos subdocumentos y tambien tener la extencion o herencia de Document
 }
 
 
@@ -36,7 +38,13 @@ const ProjectSchema: Schema = new Schema({
         require: true,
         trim: true
     },
-})
+    tasks:[//Un proyecto va a tener multiples tareas
+        {
+            type: Types.ObjectId,
+            ref: 'Task'
+        }
+    ]
+},{timestamps:true})
 
 
 //Para conectarse-- definimos nuestro modelo y se registra en la instancia de mongoose
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,9 +1,11 @@
-import mongoose, {Schema, Document} from "mongoose";
+//Creando modelos para las Tareas-Task
+import mongoose, {Schema, Document, Types} from "mongoose";
 
 //interface
 export interface ITask extends Document  {
     name: string
     description: string
+    project:Types.ObjectId
 }
 
 export const TaskSchema : Schema = new Schema({
@@ -17,8 +19,12 @@ export const TaskSchema : Schema = new Schema({
         trim: true,
         require:true
     },
+    project:{
+        type: Types.ObjectId,
+        ref: 'Project'//Pasarle la referencia de donde va a econtrar el resto de la informacion de esa tarea-- le decimos que nos creé el documento con el nombre de Project, tiene que ser un objectid y su referencia va a ser el modelo de proyecto el cual colocamos
+    }
 
-})
+},{timestamps:true})//timestamps-registra cuando fue la ultima actualización
 //conexion de schema con el interface('Nombre del modelo que le quieras poner', TaskSchema)
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
