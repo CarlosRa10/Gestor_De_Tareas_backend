24e32a5efa37a76ca9046b2657f56aa6a2da991f
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-10-31 15:46:22 -0400

    commit

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@
 import express from 'express';//Librería para crear el servidor y manejar las solicitudes HTTP.
 import dotenv from 'dotenv'//Permite cargar variables de entorno desde un archivo .env a process.env.
 import cors from 'cors'
+import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';// Importa la función de conexión a la base de datos desde db.ts.
 import projectRoutes from './routes/projectRoutes'
@@ -16,7 +17,14 @@ const app = express()//La instancia de app de Express se exporta desde server.ts
 
 app.use(cors(corsConfig)) //y una vez que geramos la aplicación permitimos las conexiones- le pasamos la config que se acaba de crear
 
+//Logging
+app.use(morgan('dev'))//se manda a utilizar morgan
+
+
+
+
 //Habilitar este tipo de formato - asi leera los valores que le enviemos en el body de tipo json
+//Leer datos de formularios
 app.use(express.json())
 
 
