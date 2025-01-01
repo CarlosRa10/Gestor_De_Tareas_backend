5767cb3e804910b8c6c2fa9b5fd0a86d1e871f5c
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-12-31 23:58:09 -0400

    commit

---
+++ src/middleware/auth.ts
@@ -0,0 +1,15 @@
+import {Request,Response,NextFunction} from 'express'
+
+export const authenticate = async (req:Request, res:Response, next:NextFunction) => {
+    //console.log(req.headers.authorization)
+    const bearer = req.headers.authorization
+    if(!bearer){
+        const error = new Error('No Autorizado')
+        res.status(401).json({error: error.message})
+        return
+    }
+    console.log(bearer)
+    next()
+}
+
+//Es una peticion htpp - primero va la URL, luego el header y despues el body
