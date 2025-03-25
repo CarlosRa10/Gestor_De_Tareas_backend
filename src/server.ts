270932e9c42e5bd8ac4d7eb6e4701e64dee5bad5
carlosra10 <carloseduardoramirez10@gmail.com>
2025-03-25 00:55:57 -0400

    act

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+//server.ts es el punto de entrada principal. Al llamarlo allí, te aseguras de que todas las variables de entorno estén disponibles para cualquier archivo que las necesite.
+//config del servidor
+import express from 'express';//Librería para crear el servidor y manejar las solicitudes HTTP.
+import dotenv from 'dotenv'//Permite cargar variables de entorno desde un archivo .env a process.env.
+import cors from 'cors'
 import morgan from 'morgan';
 import { corsConfig } from './config/cors';
-import { connectDB } from './config/db';
-import authRoutes from './routes/authRoutes';
-import projectRoutes from './routes/projectRoutes';
+import { connectDB } from './config/db';// Importa la función de conexión a la base de datos desde db.ts.
+import authRoutes from './routes/authRoutes'
+import projectRoutes from './routes/projectRoutes'
+//import router from './routes/projectRoutes';
 
-dotenv.config();
-connectDB();
+dotenv.config()
 
-const app = express();
+connectDB()//db.ts a server.ts:-La función connectDB se importa en server.ts y se utiliza para conectar a MongoDB cuando se inicia el servidor.
 
-app.use(cors(corsConfig));
-app.use(morgan('dev'));
-app.use(express.json());
+const app = express()//La instancia de app de Express se exporta desde server.ts y se importa en index.ts, donde se inicia el servidor y se define el puerto de escucha.
 
-// Rutas principales
-app.get('/', (req, res) => res.send('API Gestor de Tareas'));
-app.get('/health', (req, res) => res.status(200).send('OK'));
+app.use(cors(corsConfig)) //y una vez que geramos la aplicación permitimos las conexiones- le pasamos la config que se acaba de crear
+//"dev:api": "nodemon --exec ts-node src/index.ts --api" arrancar el servidor con este argumento --api
+//Logging
+app.use(morgan('dev'))//se manda a utilizar morgan
 
-// Rutas de la API
-app.use('/api', authRoutes); // Todas las rutas de authRoutes empezarán con /api
-app.use('/api/projects', projectRoutes);
+//Habilitar este tipo de formato - asi leera los valores que le enviemos en el body de tipo json
+//Leer datos de formularios
+app.use(express.json())
 
-export default app;
+
+//Routes--El .use porque va a soportar ese router todos los diferentes metodos o todos los verbos HTTP, delte, get, post etc
+//cuando se llama cada ruta (izquierda)  se ejecuta (derecha)
+app.use('/api/auth',authRoutes)//queda aagrupado todo lo relacionado con authRoutes en '/api/auth'
+app.use('/api/projects',projectRoutes)
+
+export default app
